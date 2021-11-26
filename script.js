f33f627ae611a8cdeb494da9f3cbf844bfa483c8
Kerensivira <92549819+Kerensivira@users.noreply.github.com>
2021-11-26 12:47:51 -0500

    5

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 
 const slider = document.querySelector("#slider");
 let slidersection = document.querySelectorAll("slider-section");
+let slidersectionLast = slidersection[slidersection.length-1];
+
+//BTN//
+
+const btnDer = document.querySelector("#derecha");
+const btnIzq = document.querySelector("#izquierda");
