26f9db42d140e927fa772686322c25270f82a3aa
Alexandra Sivira <kerensivira@gmail.com>
2022-03-04 23:23:24 -0500

    jn

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//slider//
-
-const slider = document.querySelector("#slider");
-let slidersection = document.querySelectorAll("slider-section");
-let slidersectionLast = slidersection[slidersection.length-1];
-
-//BTN//
-
-const btnDer = document.querySelector("#derecha");
-const btnIzq = document.querySelector("#izquierda");
-
-console.log ("hello")
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH = `https://image.tmdb.org/t/p/w1280`
+const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
